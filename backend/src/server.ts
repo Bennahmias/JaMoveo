17a9c9806278dc1929037923c922fc1f2e166a3d
Ben Nahmias <benna2@mta.ac.il>
2025-06-04 22:07:37 +0300

    remove for debug

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,21 @@
 import dotenv from "dotenv";
 import path from "path";
-import { loadSongs } from "./utils/songLoader";
+// import { loadSongs } from "./utils/songLoader";
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 import { httpServer } from "./app";
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  console.error("FATAL ERROR: MONGODB_URI is not defined.");
-  process.exit(1); 
-}
+// if (!MONGODB_URI) {
+//   console.error("FATAL ERROR: MONGODB_URI is not defined.");
+//   process.exit(1); 
+// }
 
 // Load songs before starting the server
 try {
-  loadSongs(); 
-  console.log("Songs loaded successfully. Starting server...");
+  // loadSongs(); 
+  // console.log("Songs loaded successfully. Starting server...");
 
   httpServer.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
